8d488841b0ba78f015de73b90767d38cfd9c372e
Razvan Popovici <razvan@tarponsoftware.com>
2024-06-02 12:54:44 -0400

    Capturing image works.

--- selenium/src/main/java/com/example/Test.java
+++ selenium/src/main/java/com/example/Test.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
 import java.nio.file.Files;
@@ -19,15 +20,36 @@ public class Test {
         return By.xpath ("//*[contains(text(),'"+text+"')]");
     }
 
-    public static void main(String[] args) {
-        try {
-            // Connect to the Selenium server running in Docker
-            URL url = new URI("http://selenium:4444/wd/hub").toURL();
-//            DesiredCapabilities capabilities = DesiredCapabilities.();
-            WebDriver driver = new RemoteWebDriver(url, new ChromeOptions());
 
-            // Navigate to Google
-            driver.get("http://maincontainer:8080");
+    public static void saveFile(WebDriver driver, String where) {
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        File f = new File(where);
+        try {
+            Files.move(scrFile.toPath(), f.toPath());
+            scrFile.delete();
+        } catch (IOException e1) {
+            throw new RuntimeException(e1);
+        }
+        if (!f.exists())
+            throw new RuntimeException("Target file "+ f.getAbsolutePath() +" was reported created by does not exists!");
+//        System.out.println("Wrote file: " + f.getAbsolutePath());
+    }
+
+    public static void main(String[] args) {
+        WebDriver driver = null;
+        try {
+            String selenium = "http://selenium:4444/wd/hub";
+            String test = "http://maincontainer:8080";
+            if (args.length > 0)
+                selenium = args[0];
+            if (args.length > 1)
+                test = args[1];
+            // Connect to the Selenium server running in Docker
+            URL url = new URI(selenium).toURL();
+//            DesiredCapabilities capabilities = DesiredCapabilities.();
+            driver = new RemoteWebDriver(url, new ChromeOptions());
+
+            driver.get(test);
 
             // Find the search box and perform a search
             WebElement bt = driver.findElement(byText("Button 2"));
@@ -39,14 +61,14 @@ public class Test {
             WebElement bt1 = driver.findElement(byText("Button 3"));
             bt1.click();
             System.out.println("Passed!");
-            File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-            File f = new File("screenshots/picture1.png");
-            Files.move(scrFile.toPath(), f.toPath());
-            System.out.println("Wrote file: " + f.getAbsolutePath());
-            // Close the browser
+            saveFile(driver, "screenshots/picture1.png");    
             driver.quit();
+            driver = null;
         } catch (Exception e) {
             e.printStackTrace();
+            if (driver != null) 
+                saveFile(driver, "screenshots/error.png");    
+            
             System.exit(1); // error state.
         }
     }
